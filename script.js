9eac129393a00d5deca5796744e63c662385661c
morywal <mjorywal@gmail.com>
2024-05-01 18:16:31 -0700

    polish

--- script.js
+++ script.js
@@ -8,33 +8,4 @@ window.addEventListener('scroll', function() {
     }
   });
 
-  document.addEventListener('DOMContentLoaded', function() {
-    var imageContainer = document.querySelector('.image-scroll');
-    var images = imageContainer.querySelectorAll('img');
-    var currentIndex = 0;
-    var interval = 3000; // Change this to adjust the interval between scrolls (in milliseconds)
-
-    function scrollToNextImage() {
-        var currentImage = images[currentIndex];
-        var scrollAmount = currentImage.width; // Scroll amount based on current image width
-
-        // Scroll to the next image
-        imageContainer.scrollLeft += scrollAmount;
-
-        // Increment index for the next image
-        currentIndex = (currentIndex + 1) % images.length;
-
-        // If reached the last image, reset to the first image
-        if (currentIndex === 0) {
-            setTimeout(function() {
-                imageContainer.scrollLeft = 0;
-            }, interval / 2); // Delay resetting to create smoother transition
-        }
-
-        setTimeout(scrollToNextImage, interval);
-    }
-
-    // Start scrolling automatically
-    scrollToNextImage();
-});
 
